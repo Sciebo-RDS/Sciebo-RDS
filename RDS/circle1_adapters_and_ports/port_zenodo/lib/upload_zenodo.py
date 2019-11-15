5c626036431d7feaf136bf9f4231e9a4ab2264cb
Peter Heiss <peter.heiss@exchange.wwu.de>
2019-11-15 08:11:11 +0100

    changes upload_zenodo methods to static

--- RDS/circle1_adapters_and_ports/port_zenodo/lib/upload_zenodo.py
+++ RDS/circle1_adapters_and_ports/port_zenodo/lib/upload_zenodo.py
@@ -3,31 +3,35 @@ import json
 import os
 import logging
 
-class Zenodo():
-    def __init__(self):
-        self.log = logging.getLogger("")
-        self.zenodo_address = "http://sandbox.zenodo.org"
 
-    def check_token(self, api_key):
+class Zenodo(object):
+    log = logging.getLogger("")
+    zenodo_address = "http://sandbox.zenodo.org"
+
+    @classmethod
+    def check_token(cls, api_key):
         """Check the API-Token `api_key`.
-        
+
         Returns `True` if the token is correct and usable, otherwise `False`."""
-        self.log.debug("Check token: Starts")
-        r = self.get_deposition(api_key, True)
-        self.log.debug(
+        cls.log.debug("Check token: Starts")
+        r = cls.get_deposition(api_key, True)
+        cls.log.debug(
             "Check Token: Status Code: {}".format(r.status_code))
 
         return r.status_code == 200
 
-    def get_deposition(self, api_key, return_request=False):
+    @classmethod
+    def get_deposition(cls, api_key, return_request=False):
         """ Require: None
                 Optional return_request: For testing purposes, you can set this to True.
             Returns: json, Alternative: request if return_request=True 
             Description: Get all depositions for the account, which owns the api-key."""
 
-        r = requests.get(os.getenv("ZENODO_ADDRESS", default=self.zenodo_address) + "/api/deposit/depositions",
-                         params={"access_token": api_key})
-        self.log.debug(
+        r = requests.get("{}/api/deposit/depositions".format(
+            os.getenv("ZENODO_ADDRESS", default=cls.zenodo_address)
+        ),
+            params={"access_token": api_key})
+        cls.log.debug(
             "Get Depositions: Status Code: {}".format(r.status_code))
 
         if return_request:
@@ -35,19 +39,22 @@ class Zenodo():
         else:
             return r.json()
 
-    def create_new_deposition(self, api_key, return_request=False):
+    @classmethod
+    def create_new_deposition(cls, api_key, return_request=False):
         """ Require: None
             Returns: Boolean, Alternative: json if return_request=True 
             Description: Creates a new deposition. You can get the id with r.json()['id']"""
-        self.log.debug("Create new deposition: Starts")
+        cls.log.debug("Create new deposition: Starts")
 
         headers = {"Content-Type": "application/json"}
 
-        r = requests.post(os.getenv("ZENODO_ADDRESS", default=self.zenodo_address) + '/api/deposit/depositions',
-                          params={'access_token': api_key}, json={},
-                          headers=headers)
+        r = requests.post('{}/api/deposit/depositions'.format(
+            os.getenv("ZENODO_ADDRESS", default=cls.zenodo_address)
+        ),
+            params={'access_token': api_key}, json={},
+            headers=headers)
 
-        self.log.debug(
+        cls.log.debug(
             "Create new deposition: Status Code: {}".format(r.status_code))
 
         if return_request:
@@ -55,7 +62,8 @@ class Zenodo():
         else:
             return r.status_code == 200
 
-    def upload_new_file_to_deposition(self, deposition_id, path_to_file, api_key, return_request=False):
+    @classmethod
+    def upload_new_file_to_deposition(cls, deposition_id, path_to_file, api_key, return_request=False):
         """ Require:
                 A deposit id (from get_deposition or create_new_deposition; r.json()['id'])
                 A path to a file
@@ -69,16 +77,20 @@ class Zenodo():
         data = {"filename": filename}
 
         files = {'file': open(path_to_file.expanduser(), 'rb')}
-        r = requests.post(os.getenv("ZENODO_ADDRESS", default=self.zenodo_address) + '/api/deposit/depositions/%s/files' % deposition_id,
-                          params={'access_token': api_key}, data=data,
-                          files=files)
+        r = requests.post('{}/api/deposit/depositions/{}/files'.format(
+            os.getenv("ZENODO_ADDRESS",
+                      default=cls.zenodo_address), deposition_id
+        ),
+            params={'access_token': api_key}, data=data,
+            files=files)
 
         if return_request:
             return r.json()
         else:
             return r.status_code == 201
 
-    def change_metadata_in_deposition(self, deposition_id, data, api_key):
+    @classmethod
+    def change_metadata_in_deposition(cls, deposition_id, data, api_key):
         """ Require:
                 A deposit id (from get_deposition or create_new_deposition; r.json()['id'])
                 A data-dict json-like object
@@ -97,14 +109,21 @@ class Zenodo():
             Description: Set the metadata to the given data or changes the values to the corresponding keys."""
         headers = {"Content-Type": "application/json"}
 
-        r = requests.put(os.getenv("ZENODO_ADDRESS", default=self.zenodo_address) + '/api/deposit/depositions/%s' % deposition_id,
-                         params={'access_token': api_key}, data=json.dumps(data),
-                         headers=headers)
+        r = requests.put('{}/api/deposit/depositions/{}'.format(
+            os.getenv("ZENODO_ADDRESS",
+                      default=cls.zenodo_address), deposition_id
+        ),
+            params={'access_token': api_key}, data=json.dumps(data),
+            headers=headers)
         return r.status_code == 200
 
-    def publish_deposition(self, deposition_id, api_key):
-        r = requests.post(os.getenv("ZENODO_ADDRESS", default=self.zenodo_address) + '/api/deposit/depositions/%s/actions/publish' % deposition_id,
-                          params={'access_token': api_key})
+    @classmethod
+    def publish_deposition(cls, deposition_id, api_key):
+        r = requests.post('{}/api/deposit/depositions/{}/actions/publish'.format(
+            os.getenv("ZENODO_ADDRESS",
+                      default=cls.zenodo_address), deposition_id
+        ),
+            params={'access_token': api_key})
 
         return r.status_code == 202
 
