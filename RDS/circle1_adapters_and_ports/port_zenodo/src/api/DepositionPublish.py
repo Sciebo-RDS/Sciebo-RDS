8fb720d52186aab6cc63b9d9c34204cc8e6a064f
Peter Heiss <peter.heiss@exchange.wwu.de>
2019-11-20 15:20:43 +0100

    implements publish

--- RDS/circle1_adapters_and_ports/port_zenodo/src/api/DepositionPublish.py
+++ RDS/circle1_adapters_and_ports/port_zenodo/src/api/DepositionPublish.py
@@ -1,6 +1,14 @@
+import os
+from lib.upload_zenodo import Zenodo
+from flask import request
 
-def index():
-    pass
+z = Zenodo(os.getenv("ZENODO_API_KEY"))
 
-def put():
-    pass
+def index(deposition_id):
+    r = z.get_deposition(return_response=True)
+    v = r["submitted"]
+    return v, r.status_code
+
+def put(deposition_id):
+    r = z.publish_deposition_internal(deposition_id, return_response=True)
+    return r.status_code == 202, r.status_code
