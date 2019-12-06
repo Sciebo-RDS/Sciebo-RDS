e3e8b8d11754ad25a9166a4d061c25dc395ef308
Peter Heiss <peter.heiss@uni-muenster.de>
2019-12-06 13:51:39 +0100

    remove deprecated code

--- RDS/circle3_central_services/token_service/src/api/Token/Token.py
+++ RDS/circle3_central_services/token_service/src/api/Token/Token.py
@@ -2,6 +2,7 @@ from flask import jsonify
 from lib.Token import Token, OAuth2Token
 import Util
 
+
 def index():
     tokens = Util.storage.getTokens()
     data = {
@@ -11,29 +12,15 @@ def index():
     return jsonify(data)
 
 # TODO Implements me
+
+
 def get(token_id):
     pass
+
 
 def put(token_id):
     pass
 
+
 def post():
     pass
-
-def get_token_object(jsonStr):
-    try:
-        return OAuth2Token.from_dict(request.json)
-    except ValueError:
-        pass
-
-    try:
-        return Token.from_dict(request.json)
-    except ValueError:
-        pass
-
-    try:
-        return Util.initialize_object_from_json(request.json)
-    except ValueError:
-        pass
-
-    raise ValueError("Cannot interpret given string as service.")
