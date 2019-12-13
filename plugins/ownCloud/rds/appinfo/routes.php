13c0f3058423028767411bf52580766294a0253d
Peter Heiss <peter.heiss@uni-muenster.de>
2019-12-13 17:48:33 +0100

    rename service to service_api in route

--- plugins/ownCloud/rds/appinfo/routes.php
+++ plugins/ownCloud/rds/appinfo/routes.php
@@ -8,9 +8,9 @@ $application->registerRoutes($this, [
         ['name' => 'page#index',                            'url' => '/',                                       'verb' => 'GET'],
 
         # API endpoints
-        ['name' => 'service#index',                         'url' => '/api/v1/service',                         'verb' => 'GET'],
-        ['name' => 'service#show',                          'url' => '/api/v1/service/{servicename}',           'verb' => 'GET'],
-        ['name' => 'service#removeServiceFromUser',         'url' => '/api/v1/service/{servicename}/delete',    'verb' => 'POST'],
-        ['name' => 'service#getRegisteredServicesForUser',  'url' => '/api/v1/user/service',                    'verb' => 'GET'],
+        ['name' => 'service_api#index',                         'url' => '/api/v1/service',                         'verb' => 'GET'],
+        ['name' => 'service_api#show',                          'url' => '/api/v1/service/{servicename}',           'verb' => 'GET'],
+        ['name' => 'service_api#removeServiceFromUser',         'url' => '/api/v1/service/{servicename}/delete',    'verb' => 'POST'],
+        ['name' => 'service_api#getRegisteredServicesForUser',  'url' => '/api/v1/user/service',                    'verb' => 'GET'],
     ]
 ]);
