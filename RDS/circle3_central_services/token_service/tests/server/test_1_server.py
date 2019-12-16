fd51e8317872a75096fd8b11f6ba56470c7166db
Peter Heiss <peter.heiss@uni-muenster.de>
2019-12-16 13:42:19 +0100

    remove single line

--- RDS/circle3_central_services/token_service/tests/server/test_1_server.py
+++ RDS/circle3_central_services/token_service/tests/server/test_1_server.py
@@ -13,8 +13,6 @@ import Util
 
 
 def create_app():
-    # set var for mock service
-    os.environ["address"] = "http://localhost:5000"
     # creates a test client
     app = bootstrap(use_default_error=True).app
     # propagate the exceptions to the test client
