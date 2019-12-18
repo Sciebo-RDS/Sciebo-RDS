23c002b95b50b838b8fc78d5c17332ac21792592
Peter Heiss <peter.heiss@uni-muenster.de>
2019-12-18 15:44:14 +0100

    small changes

--- plugins/ownCloud/Readme.md
+++ plugins/ownCloud/Readme.md
@@ -1,11 +1,15 @@
 
+# ownCloud Plugin for RDS
+
 This plugin enables RDS in your owncloud installation.
 
-# Installation
+## Installation
 
-Copy the files into the folder `{your-owncloud-installation}/apps/rds`. Activate it in your admin apps panel.
+Copy the folder `rds` to your ownCloud `apps/`. Enable the plugin in your administration center in your webinterface.
+Example: Copy all files in `rds` to `/var/www/html/owncloud/apps/rds`.
 
 # Dependencies
 
 - owncloud 10
 - php-curl
+- oauth2 plugin in your owncloud installation
--- plugins/ownCloud/rds/js/authorizeRDS.js
+++ plugins/ownCloud/rds/js/authorizeRDS.js
@@ -5,7 +5,7 @@
     function openAuthorizeOwncloud() {
       window.location.href =
         OC.generateUrl("/apps/oauth2/authorize") +
-        "?response_type=code&client_id=S4MQ9MjTqb2sV47noTsQJ6REijG0u0LkScWJA2VG3LHkq7ue5t3CQPlu4ypX7RkS&redirect_uri=http://sciebords-dev.uni-muenster.de/oauth2/redirect";
+        "?response_type=code&client_id=DjuJWDG8xzXYu5ttjaKoY0KPfRKcVmhvdkt6u2Y2KZq8fFb5i2FtRiBQmBDJgAIM&redirect_uri=http://sciebords-dev.uni-muenster.de/token-service/redirect";
     }
 
     var button = document.getElementById("openAuthorizeOwncloud");
