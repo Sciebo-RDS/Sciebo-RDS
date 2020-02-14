7f831ff13d05e10017afcf0a4f0ffc1787437e33
Peter Heiss <peter.heiss@uni-muenster.de>
2020-02-14 16:03:40 +0100

    refix

--- RDS/circle2_use_cases/exporter/src/api/export.py
+++ RDS/circle2_use_cases/exporter/src/api/export.py
@@ -5,13 +5,13 @@ import logging
 logger = logging.getLogger()
 
 def post(toService):
-    json = request.values
+    json = request.get_json(force=True)
     userId = json.get("user_id")
     fromService = json.get("from_service")
     filepath = json.get("filename")
 
     result = ExporterService(testing=current_app.config.get("TESTING", False), testing_address=current_app.config.get(
-        "TESTSERVER", None)).export(fromService, toService, filepath, userId)
+        "TESTSERVER")).export(fromService, toService, filepath, userId)
 
     if result:
         return jsonify({"success": True})
