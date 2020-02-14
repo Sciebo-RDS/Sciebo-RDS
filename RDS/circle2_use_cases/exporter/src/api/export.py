1b3700d29eec6b30c575fd40aaf41b6ec12ac48b
Peter Heiss <peter.heiss@uni-muenster.de>
2020-02-14 15:53:09 +0100

    change implementation to x-www-urlencoded

--- RDS/circle2_use_cases/exporter/src/api/export.py
+++ RDS/circle2_use_cases/exporter/src/api/export.py
@@ -5,7 +5,7 @@ import logging
 logger = logging.getLogger()
 
 def post(toService):
-    json = request.json
+    json = request.values
     userId = json.get("user_id")
     fromService = json.get("from_service")
     filepath = json.get("filename")
