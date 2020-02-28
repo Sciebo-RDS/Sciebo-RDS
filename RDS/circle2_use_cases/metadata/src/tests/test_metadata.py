1ca290a1e929bee0bdb9918a57ff58eedf9ac4cc
Peter Heiss <peter.heiss@uni-muenster.de>
2020-02-28 14:01:06 +0100

    insert pactman to metadata tests

--- RDS/circle2_use_cases/metadata/src/tests/test_metadata.py
+++ RDS/circle2_use_cases/metadata/src/tests/test_metadata.py
@@ -1,7 +1,13 @@
 from src.lib.Metadata import Metadata
 import unittest
+from pactman import Consumer, Provider
 
 # TODO: use pactman for requests
+
+pact = Consumer('UseCaseMetadata').has_pact_with(
+    Provider('PortMetadata'), port=3000)
+
+testing_address = "http://localhost:3000"
 
 
 class Test_Metadata(unittest.TestCase):
@@ -10,6 +16,18 @@ class Test_Metadata(unittest.TestCase):
         This unit tests the metadata object constructor.
         """
         pass
+
+        # test returned state jwt object
+        pact.given(
+            'An oauthservice was registered.'
+        ).upon_receiving(
+            'A request to get this oauthservice.'
+        ).with_request(
+            'GET', f"/service/{service.servicename}"
+        ) .will_respond_with(200, body=service.to_json())
+
+        with pact:
+            pass
 
     def test_metadata_get_projectid(self):
         """
