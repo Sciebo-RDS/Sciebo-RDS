cd30dceebf295b43d41dd113c909af2e85038fc7
Peter Heiss <peter.heiss@uni-muenster.de>
2020-03-19 13:43:49 +0100

    add missing parameter

--- RDS/circle3_central_services/token_service/tests/server/test_5_end_to_end.py
+++ RDS/circle3_central_services/token_service/tests/server/test_5_end_to_end.py
@@ -94,8 +94,8 @@ class test_end_to_end(unittest.TestCase):
 
             req = requests.post(owncloud.refresh_url, data=data, auth=(
                 owncloud.client_id, owncloud.client_secret)).json()
-            oauthtoken = OAuth2Token(
-                owncloud, req["access_token"], req["refresh_token"], datetime.now() + timedelta(seconds=req["expires_in"]))
+            oauthtoken = OAuth2Token(user, owncloud, req["access_token"], req["refresh_token"], datetime.now(
+            ) + timedelta(seconds=req["expires_in"]))
             return oauthtoken
 
         oauthtoken1 = get_access_token(user1, token1)
