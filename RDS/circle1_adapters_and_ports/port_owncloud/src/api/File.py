156a38cbd8a7d2173beac94f537088ff8af228d1
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-06 16:08:35 +0200

    port owncloud returns file

--- RDS/circle1_adapters_and_ports/port_owncloud/src/api/File.py
+++ RDS/circle1_adapters_and_ports/port_owncloud/src/api/File.py
@@ -6,18 +6,35 @@ from connexion_plus import FlaskOptimize
 
 logger = logging.getLogger()
 
+
 @FlaskOptimize.do_not_minify()
 @FlaskOptimize.do_not_compress()
-def get(filepath):
+def index(filepath):
     userId = request.values.get("userId")
     logger.debug(f"userid {userId}")
 
     import os
     file = OwncloudUser(userId).getFile(filepath)
-    
+
     rv = send_file(file, attachment_filename=os.path.basename(
         filepath), as_attachment=True, mimetype="application/octet-stream")
     logger.debug("disable passthrough")
     rv.direct_passthrough = False
     logger.debug("send response")
     return rv
+
+
+@FlaskOptimize.do_not_minify()
+@FlaskOptimize.do_not_compress()
+def patch(filepath):
+    raise NotImplementedError()
+
+
+@FlaskOptimize.do_not_minify()
+@FlaskOptimize.do_not_compress()
+def post(filepath):
+    raise NotImplementedError()
+
+
+def delete(filepath):
+    raise NotImplementedError()
