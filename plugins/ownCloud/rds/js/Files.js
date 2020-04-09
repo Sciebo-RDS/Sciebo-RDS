c4673ee9cfc27016f7d901f7fc3636e9f6f0a86a
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-09 12:57:57 +0200

    remove ready function for javascript

--- plugins/ownCloud/rds/js/Files.js
+++ plugins/ownCloud/rds/js/Files.js
@@ -1,8 +1,6 @@
 (function (OC, window, $, undefined) {
   "use strict";
-  $(document).ready(function () {
-    var Files = function (baseUrl) {
-      this._baseUrl = baseUrl;
-    };
-  });
+  var Files = function (baseUrl) {
+    this._baseUrl = baseUrl;
+  };
 })(OC, window, jQuery);
--- plugins/ownCloud/rds/js/Studies.js
+++ plugins/ownCloud/rds/js/Studies.js
@@ -1,112 +1,110 @@
 (function (OC, window, $, undefined) {
   "use strict";
 
-  $(document).ready(function () {
-    var Studies = function (baseUrl, metadata) {
-      this._baseUrl = baseUrl;
-      this._studies = [];
-      this._activeResearch = undefined;
-      this._metadata = metadata;
-    };
+  var Studies = function (baseUrl, metadata) {
+    this._baseUrl = baseUrl;
+    this._studies = [];
+    this._activeResearch = undefined;
+    this._metadata = metadata;
+  };
 
-    Studies.prototype = {
-      load: function (researchIndex) {
-        var self = this;
-        this._studies.forEach(function (conn) {
-          if (conn.researchIndex === researchIndex) {
-            conn.active = true;
-            self._activeResearch = conn;
-            metadata.load(researchIndex);
-          } else {
-            conn.active = false;
-          }
-        });
-      },
-      getActive: function () {
-        return this._activeResearch;
-      },
-      removeActive: function () {
-        var index;
-        var deferred = $.Deferred();
-        var researchIndex = this._activeResearch.researchIndex;
-
-        this._studies.forEach(function (conn, counter) {
-          if (conn.researchIndex === researchIndex) {
-            index = counter;
-          }
-        });
-
-        if (index !== undefined) {
-          if (this._activeResearch === this._studies[index]) {
-            delete this._activeResearch;
-          }
-
-          this._studies.splice(index, 1);
-
-          $.ajax({
-            url: this._baseUrl + "/" + researchIndex,
-            method: "DELETE",
-          })
-            .done(function () {
-              deferred.resolve();
-            })
-            .fail(function () {
-              deferred.reject();
-            });
+  Studies.prototype = {
+    load: function (researchIndex) {
+      var self = this;
+      this._studies.forEach(function (conn) {
+        if (conn.researchIndex === researchIndex) {
+          conn.active = true;
+          self._activeResearch = conn;
+          metadata.load(researchIndex);
         } else {
-          deferred.reject();
+          conn.active = false;
         }
-        return deferred.promise();
-      },
-      create: function () {
-        var deferred = $.Deferred();
-        var self = this;
+      });
+    },
+    getActive: function () {
+      return this._activeResearch;
+    },
+    removeActive: function () {
+      var index;
+      var deferred = $.Deferred();
+      var researchIndex = this._activeResearch.researchIndex;
+
+      this._studies.forEach(function (conn, counter) {
+        if (conn.researchIndex === researchIndex) {
+          index = counter;
+        }
+      });
+
+      if (index !== undefined) {
+        if (this._activeResearch === this._studies[index]) {
+          delete this._activeResearch;
+        }
+
+        this._studies.splice(index, 1);
+
         $.ajax({
-          url: this._baseUrl,
-          method: "POST",
-        })
-          .done(function (conn) {
-            self._studies.push(conn);
-            self._activeResearch = conn;
-            self.load(conn.researchIndex);
-            deferred.resolve();
-          })
-          .fail(function () {
-            deferred.reject();
-          });
-        return deferred.promise();
-      },
-      getAll: function () {
-        return this._studies;
-      },
-      loadAll: function () {
-        var deferred = $.Deferred();
-        var self = this;
-        $.get(this._baseUrl)
-          .done(function (conns) {
-            self._activeResearch = undefined;
-            self._studies = conns;
-            deferred.resolve();
-          })
-          .fail(function () {
-            deferred.reject();
-          });
-        return deferred.promise();
-      },
-      updateActive: function (researchIndex, status, portIn, portOut) {
-        var conn = this.getActive();
-
-        conn.status = status;
-        conn.portIn = portIn;
-        conn.portOut = portOut;
-
-        return $.ajax({
           url: this._baseUrl + "/" + researchIndex,
-          method: "PUT",
-          contentType: "application/json",
-          data: JSON.stringify(conn),
+          method: "DELETE",
+        })
+          .done(function () {
+            deferred.resolve();
+          })
+          .fail(function () {
+            deferred.reject();
+          });
+      } else {
+        deferred.reject();
+      }
+      return deferred.promise();
+    },
+    create: function () {
+      var deferred = $.Deferred();
+      var self = this;
+      $.ajax({
+        url: this._baseUrl,
+        method: "POST",
+      })
+        .done(function (conn) {
+          self._studies.push(conn);
+          self._activeResearch = conn;
+          self.load(conn.researchIndex);
+          deferred.resolve();
+        })
+        .fail(function () {
+          deferred.reject();
         });
-      },
-    };
-  });
+      return deferred.promise();
+    },
+    getAll: function () {
+      return this._studies;
+    },
+    loadAll: function () {
+      var deferred = $.Deferred();
+      var self = this;
+      $.get(this._baseUrl)
+        .done(function (conns) {
+          self._activeResearch = undefined;
+          self._studies = conns;
+          deferred.resolve();
+        })
+        .fail(function () {
+          deferred.reject();
+        });
+      return deferred.promise();
+    },
+    updateActive: function (researchIndex, status, portIn, portOut) {
+      var conn = this.getActive();
+
+      conn.status = status;
+      conn.portIn = portIn;
+      conn.portOut = portOut;
+
+      return $.ajax({
+        url: this._baseUrl + "/" + researchIndex,
+        method: "PUT",
+        contentType: "application/json",
+        data: JSON.stringify(conn),
+      });
+    },
+  };
 })(OC, window, jQuery);
