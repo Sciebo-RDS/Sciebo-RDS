507b6421f44b747631c726dcf594b1dce47bcd95
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-15 14:11:44 +0200

    init controller in app plugin oc

--- plugins/ownCloud/rds/appinfo/application.php
+++ plugins/ownCloud/rds/appinfo/application.php
@@ -3,9 +3,21 @@ namespace OCA\RDS\AppInfo;
 
 use \OCP\AppFramework\App;
 use \OCA\RDS\Controller\PageController;
+
 use \OCA\RDS\Controller\ServiceController;
+use \OCA\RDS\Db\ServiceMapper;
+use \OCA\RDS\Service\ServiceportService;
+
 use \OCA\RDS\Controller\ResearchController;
+use \OCA\RDS\Service\ResearchService;
+use \OCA\RDS\Db\ResearchMapper;
+
 use \OCA\RDS\Controller\MetadataController;
+use \OCA\RDS\Service\MetadataService;
+use \OCA\RDS\Db\MetadataMapper;
+
+
+
 
 class Application extends App {
     public function __construct(array $urlParams=array()){
@@ -22,26 +34,38 @@ class Application extends App {
             );
         });
 
+        $servicemapper = new ServiceMapper();
+        $serviceportservice = new ServiceportService($servicemapper);
+
         $container->registerService('ServiceController', function($c) {
             return new ServiceController(
                 $c->query('AppName'),
                 $c->query('Request'),
+                $serviceportservice,
                 $c->query('UserId')
             );
         });
+
+        $researchmapper = new ResearchMapper();
+        $researchservice = new ResearchService($researchmapper);
 
         $container->registerService('ResearchController', function($c) {
             return new ResearchController(
                 $c->query('AppName'),
                 $c->query('Request'),
+                $researchservice,
                 $c->query('UserId')
             );
         });
+
+        $metadatamapper = new MetadataMapper();
+        $metadataservice = new MetadataService($metadatamapper);
 
         $container->registerService('MetadataController', function($c) {
             return new MetadataController(
                 $c->query('AppName'),
                 $c->query('Request'),
+                $metadataservice,
                 $c->query('UserId')
             );
         });
