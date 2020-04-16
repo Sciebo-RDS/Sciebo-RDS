bed7fefe91b0e6e1bc92ccc8cf75f8ef030f35d9
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-16 13:47:08 +0200

    revert latest commit

--- plugins/ownCloud/rds/db/researchmapper.php
+++ plugins/ownCloud/rds/db/researchmapper.php
@@ -19,9 +19,6 @@ class ResearchMapper {
         curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
         curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, false );
 
-        # FIXME: remove me, because this is for self signed certs
-        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);
-
         $response = json_decode( curl_exec( $curl ), true );
         $httpcode = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
         $info = curl_getinfo( $curl );
@@ -55,9 +52,6 @@ class ResearchMapper {
         curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
         curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, false );
 
-        # FIXME: remove me, because this is for self signed certs
-        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);
-
         $response = json_decode( curl_exec( $curl ) );
         $httpcode = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
         $info = curl_getinfo( $curl );
@@ -83,9 +77,6 @@ class ResearchMapper {
         curl_setopt_array( $curl, $options );
         curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
         curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, false );
-
-        # FIXME: remove me, because this is for self signed certs
-        curl_setopt ( $curl, CURLOPT_SSL_VERIFYPEER, false);
 
         $response = json_decode( curl_exec( $curl ) );
         $httpcode = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
