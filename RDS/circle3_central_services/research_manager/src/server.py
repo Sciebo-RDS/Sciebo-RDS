6ceb8f1d0fcbe2034bc3e1da79ed3ef1874cb8da
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-20 20:52:52 +0200

    fixed research mgr jaeger

--- RDS/circle3_central_services/research_manager/src/server.py
+++ RDS/circle3_central_services/research_manager/src/server.py
@@ -4,5 +4,5 @@ from __init__ import bootstrap
 
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
-app = bootstrap("UseCaseMetadata", all=True)
+app = bootstrap("CentralServiceResearch", all=True)
 app.run(port=8080, server='gevent')
