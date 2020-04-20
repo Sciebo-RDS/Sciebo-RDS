1e062142de86c9d14ddaa42d8b4068dece69dc8b
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-20 21:01:10 +0200

    add log to research mgr

--- RDS/circle3_central_services/research_manager/src/api/User/Research/Imports/port.py
+++ RDS/circle3_central_services/research_manager/src/api/User/Research/Imports/port.py
@@ -4,18 +4,21 @@ from flask import jsonify, request
 import logging
 logger = logging.getLogger()
 
+
 def index(user_id, research_id):
     return jsonify(Singleton.ProjectService.getProject(user_id, int(research_id)).getPortIn())
 
 
 def post(user_id, research_id):
     json = request.json
+    logger.debug(f"got json: {json}")
     from lib.Port import Port
 
+    portname = json["port"]
     fs = False
     md = False
     cp = None
-    
+
     for prop in json.get("properties", []):
         if prop.get("portType", "") == "fileStorage":
             fs = prop["value"]
@@ -24,7 +27,10 @@ def post(user_id, research_id):
         elif prop.get("portType", "") == "customProperties":
             cp = prop["value"]
 
-    p = Port(json["port"], fileStorage=fs, metadata=md, customProperties=cp)
+    logger.debug(
+        f"parsed data: port: {portname}, fs: {fs}, metadata: {md}, cp: {cp}")
+
+    p = Port(portname, fileStorage=fs, metadata=md, customProperties=cp)
 
     Singleton.ProjectService.getProject(user_id, int(research_id)).addPortIn(p)
     return jsonify(Singleton.ProjectService.getProject(user_id, int(research_id)).getPortIn())
@@ -35,5 +41,6 @@ def get(user_id, research_id, port_id):
 
 
 def delete(user_id, research_id, port_id):
-    Singleton.ProjectService.getProject(user_id, int(research_id)).removePortIn(int(port_id))
+    Singleton.ProjectService.getProject(
+        user_id, int(research_id)).removePortIn(int(port_id))
     return jsonify(Singleton.ProjectService.getProject(user_id, int(research_id)).getPortIn())
