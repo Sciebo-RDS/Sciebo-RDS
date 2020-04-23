e978b7811977c823bd620d5c9256f269ec83d653
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-23 16:35:17 +0200

    add wrapper

--- plugins/ownCloud/rds/templates/part.research.content.file.php
+++ plugins/ownCloud/rds/templates/part.research.content.file.php
@@ -1,15 +1,19 @@
 this are the fields of files
 
-<label>
-    <input type="checkbox" id="checkbox-automatic-upload">
-    <?php p($l->t('Files auto upload')); ?>
-    <a href="#" id="helper-auto-upload" title="<?php p($l->t('The system will upload all files in the given folder after some time.')); ?>"><img src="core/img/actions/info.svg" /></a>
-</label>
-<label>
-    <input type="checkbox" id="checkbox-apply-changes">
-    <?php p($l->t('Apply file changes')); ?>
-    <a href="#" id="helper-apply-changes" title="<?php p($l->t('Otherwise deletions or file changes are not synchronized to the given services.')); ?>"><img src="core/img/actions/info.svg" /></a>
-</label>
+<div id="wrapper-auto-upload">
+    <label>
+        <input type="checkbox" id="checkbox-automatic-upload">
+        <?php p($l->t('Files auto upload')); ?>
+        <a href="#" id="helper-auto-upload" title="<?php p($l->t('The system will upload all files in the given folder after some time.')); ?>"><img src="/core/img/actions/info.svg" /></a>
+    </label>
+</div>
+<div id="wrapper-apply-changes">
+    <label>
+        <input type="checkbox" id="checkbox-apply-changes">
+        <?php p($l->t('Apply file changes')); ?>
+        <a href="#" id="helper-apply-changes" title="<?php p($l->t('Otherwise deletions or file changes are not synchronized to the given services.')); ?>"><img src="/core/img/actions/info.svg" /></a>
+    </label>
+</div>
 
 <div id="wrapper-custom-buttons">
   <div id="spacer"></div>
