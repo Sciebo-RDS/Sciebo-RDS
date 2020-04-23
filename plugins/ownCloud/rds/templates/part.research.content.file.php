83877e26a577c8a50846a243cec1e6f326682619
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-23 16:37:59 +0200

    fix url for info.svg

--- plugins/ownCloud/rds/templates/part.research.content.file.php
+++ plugins/ownCloud/rds/templates/part.research.content.file.php
@@ -4,14 +4,14 @@ this are the fields of files
     <label>
         <input type="checkbox" id="checkbox-automatic-upload">
         <?php p($l->t('Files auto upload')); ?>
-        <a href="#" id="helper-auto-upload" title="<?php p($l->t('The system will upload all files in the given folder after some time.')); ?>"><img src="/core/img/actions/info.svg" /></a>
+        <a href="#" id="helper-auto-upload" title="<?php p($l->t('The system will upload all files in the given folder after some time.')); ?>"><img src="<?php print_unescaped(image_path('', 'actions/info.svg')); ?>" /></a>
     </label>
 </div>
 <div id="wrapper-apply-changes">
     <label>
         <input type="checkbox" id="checkbox-apply-changes">
         <?php p($l->t('Apply file changes')); ?>
-        <a href="#" id="helper-apply-changes" title="<?php p($l->t('Otherwise deletions or file changes are not synchronized to the given services.')); ?>"><img src="/core/img/actions/info.svg" /></a>
+        <a href="#" id="helper-apply-changes" title="<?php p($l->t('Otherwise deletions or file changes are not synchronized to the given services.')); ?>"><img src="<?php print_unescaped(image_path('', 'actions/info.svg')); ?>" /></a>
     </label>
 </div>
 
