d3032b08df67f44637b86b6408ab94ba311262b9
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-23 17:05:56 +0200

    add class tooltip

--- plugins/ownCloud/rds/templates/part.research.content.file.php
+++ plugins/ownCloud/rds/templates/part.research.content.file.php
@@ -4,14 +4,14 @@ this are the fields of files
     <label>
         <input type="checkbox" id="checkbox-automatic-upload">
         <?php p($l->t('Files auto upload')); ?>
-        <img src="<?php print_unescaped(image_path('', 'actions/info.svg')); ?>"  id="helper-auto-upload" title="<?php p($l->t('The system will upload all files in the given folder after some time.')); ?>" />
+        <img class="tooltip" src="<?php print_unescaped(image_path('', 'actions/info.svg')); ?>"  id="helper-auto-upload" title="<?php p($l->t('The system will upload all files in the given folder after some time.')); ?>" />
     </label>
 </div>
 <div id="wrapper-apply-changes">
     <label>
         <input type="checkbox" id="checkbox-apply-changes">
         <?php p($l->t('Apply file changes')); ?>
-        <img src="<?php print_unescaped(image_path('', 'actions/info.svg')); ?>"  id="helper-apply-changes" title="<?php p($l->t('Otherwise deletions or file changes are not synchronized to the given services.')); ?>" />
+        <img class="tooltip" src="<?php print_unescaped(image_path('', 'actions/info.svg')); ?>"  id="helper-apply-changes" title="<?php p($l->t('Otherwise deletions or file changes are not synchronized to the given services.')); ?>" />
     </label>
 </div>
 
