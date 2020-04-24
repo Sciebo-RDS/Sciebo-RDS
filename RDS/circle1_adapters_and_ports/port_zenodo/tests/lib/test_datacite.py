0b6b6030b07bebb2db53658a7ece0a51c04b48f2
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-24 12:39:35 +0200

    disable datacite to work with port zenodo

--- RDS/circle1_adapters_and_ports/port_zenodo/tests/lib/test_datacite.py
+++ RDS/circle1_adapters_and_ports/port_zenodo/tests/lib/test_datacite.py
@@ -5,6 +5,7 @@ from jsonschema import ValidationError
 
 
 class TestDatacite(unittest.TestCase):
+    @unittest.skip("currently not implemented")
     def testInit(self):
         d = Examples.getDataCiteExample1()
         self.assertEqual(Datacite(d).toDict(), d)
@@ -12,15 +13,18 @@ class TestDatacite(unittest.TestCase):
         with self.assertRaises(ValidationError):
             Datacite({})
 
+    @unittest.skip("currently not implemented")
     def testGet(self):
         d = Examples.getDataCiteExample1()
         self.assertNotEqual(Datacite(d).creators, [])
         self.assertEqual(Datacite(d).creators, d["creators"])
 
+    @unittest.skip("currently not implemented")
     def testTransformFromZenodo(self):
         zenodo, datacite = Examples.getZenodoExample1()
         self.assertEqual(Datacite.fromZenodoApi(zenodo), datacite)
 
+    @unittest.skip("currently not implemented")
     def testTransformToZenodo(self):
         zenodo, datacite = Examples.getZenodoExample1()
         self.assertEqual(Datacite(datacite).toZenodoApi(), zenodo)
