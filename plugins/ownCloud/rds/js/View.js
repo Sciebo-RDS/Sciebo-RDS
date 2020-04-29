146db56d2ee5673face378bf4e4522ae24f30939
Peter Heiss <peter.heiss@uni-muenster.de>
2020-04-29 21:39:03 +0200

    valid syntax

--- plugins/ownCloud/rds/js/View.js
+++ plugins/ownCloud/rds/js/View.js
@@ -323,7 +323,7 @@
       var BrutusinForms = brutusin["json-forms"];
       self._bf = BrutusinForms.create(self._studies._metadata.getSchema());
       var container = document.getElementById("metadata-jsonschema-editor");
-      container.html("");
+      container.innerHTML("");
       self._bf.render(container, data);
     });
 
