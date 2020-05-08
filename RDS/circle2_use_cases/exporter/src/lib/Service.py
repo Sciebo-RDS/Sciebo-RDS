a740f2ebecb244b4f9cddb199053e599d1762650
Peter Heiss <peter.heiss@uni-muenster.de>
2020-05-08 19:19:11 +0200

    try this revertion out now

--- RDS/circle2_use_cases/exporter/src/lib/Service.py
+++ RDS/circle2_use_cases/exporter/src/lib/Service.py
@@ -71,7 +71,7 @@ class Service():
                 content = self.getFile(index)
 
                 logger.debug("got content size: {}".format(
-                    len(content.read())))
+                    len(content.getvalue())))
 
                 yield file, content
             else:
@@ -113,7 +113,7 @@ class Service():
 
     def addFile(self, filename, fileContent):
         files = {
-            "file": (filename, fileContent.read())
+            "file": (filename, fileContent.getvalue())
         }
         data = {
             "userId": self.userId,
