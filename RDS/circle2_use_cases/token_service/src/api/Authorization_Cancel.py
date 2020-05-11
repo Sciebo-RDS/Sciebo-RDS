8d82529dcb8ffa6ad93fc220e6331c288b4e7339
Peter Heiss <peter.heiss@uni-muenster.de>
2020-05-11 21:50:59 +0200

    html respons

--- RDS/circle2_use_cases/token_service/src/api/Authorization_Cancel.py
+++ RDS/circle2_use_cases/token_service/src/api/Authorization_Cancel.py
@@ -1,4 +1,11 @@
 from flask import jsonify
 
+
 def index():
-    return "<html><body><center>There was an error. You can close this window now. <button click=\"close();\">Close</button></center</body></html>"
+    return """
+    <html>
+    <body>
+    <center>There was an error. You can close this window now. <button click=\"close();\">Close</button></center>
+    </body>
+    </html>
+    """
--- RDS/circle2_use_cases/token_service/src/api/Authorization_Success.py
+++ RDS/circle2_use_cases/token_service/src/api/Authorization_Success.py
@@ -2,4 +2,10 @@ from flask import jsonify
 
 
 def index():
-    return "<html><body><center>It works. You can close this window now. <button click=\"close();\">Close</button></center</body></html>"
+    return """
+    <html>
+    <body>
+    <center>All wents fine. You can close this window now. <button click=\"close();\">Close</button></center>
+    </body>
+    </html>
+    """
