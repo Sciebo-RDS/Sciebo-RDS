fa57f63593573165c820d2434597a89fbcb272fe
Peter Heiss <peter.heiss@uni-muenster.de>
2020-05-11 20:45:01 +0200

    disable timeout for token service c2

--- RDS/circle2_use_cases/token_service/src/api/Service/Service.py
+++ RDS/circle2_use_cases/token_service/src/api/Service/Service.py
@@ -5,6 +5,5 @@ from connexion_plus import FlaskOptimize
 def index():
     return jsonify(Util.tokenService.getAllServices())
 
-@FlaskOptimize.set_cache_timeout(3600)
 def get(servicename):
     return Util.tokenService.getService(servicename)
