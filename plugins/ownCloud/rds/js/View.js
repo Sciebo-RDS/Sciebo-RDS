c70248b70c058ea9e0b6f0679aff04a90f86cd9a
Peter Heiss <peter.heiss@uni-muenster.de>
2020-05-14 10:18:16 +0200

    reload projects in view after create a new porject to show the new project in intrface

--- plugins/ownCloud/rds/js/View.js
+++ plugins/ownCloud/rds/js/View.js
@@ -284,10 +284,15 @@
         }
       });
 
-      $.when.apply($, deferreds).always(function () {
-        self.save();
-        self._view.render();
-      });
+      $.when
+        .apply($, deferreds)
+        .done(function () {
+          self._studies.load(self._studies.getActive().researchIndex);
+        })
+        .always(function () {
+          self.save();
+          self._view.render();
+        });
     });
 
     $("#app-content #btn-save-research-and-continue").click(function () {
