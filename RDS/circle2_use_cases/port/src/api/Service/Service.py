8593b93de5d6c6067dab6a1dbaa41fb03629b7f1
Peter Heiss <peter.heiss@uni-muenster.de>
2020-05-20 12:21:54 +0200

    remove caching

--- RDS/circle2_use_cases/port/src/api/Service/Service.py
+++ RDS/circle2_use_cases/port/src/api/Service/Service.py
@@ -2,9 +2,10 @@ from flask import jsonify
 import Util
 from connexion_plus import FlaskOptimize
 
+
 def index():
     return jsonify(Util.tokenService.getAllServices())
 
-@FlaskOptimize.set_cache_timeout(10)
+
 def get(servicename):
     return Util.tokenService.getService(servicename)
