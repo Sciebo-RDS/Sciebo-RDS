97ad9c888d1cd2c1d1c8aeb6e472cec1949de03c
Peter Heiss <peter.heiss@uni-muenster.de>
2020-05-20 12:59:44 +0200

    implements exchange

--- RDS/circle2_use_cases/port/src/api/Exchange.py
+++ RDS/circle2_use_cases/port/src/api/Exchange.py
@@ -1,6 +1,42 @@
-from flask import request, redirect
+from flask import request, redirect, abort
 import Util
+import json
+import jwt
+import logging
+
+logger = logging.getLogger()
 
 
 def post():
-    pass
+    try:
+        master_jwt = request.json().get("jwt")
+        unverified = jwt.decode(master_jwt, verify=False)
+
+        servicename = unverified.get("servicename")
+        service = Util.tokenService.getService(servicename)
+
+        master_data = jwt.decode(
+            master_jwt, service.client_secret, algorithms="HS256")
+
+        logger.debug("jwt: {}, decoded: {}".format(master_jwt, master_data))
+
+        userId = master_data.get("userId")
+        code = master_data.get("code")
+        logger.debug("code: {}, userId: {}".format(code, userId))
+
+        state_jwt = master_data.get("state")
+        state_data = jwt.decode(
+            state_jwt, Util.tokenService.secret, algorithms="HS256")
+
+        logger.debug("state: {}, decoded state: {}".format(
+            state_jwt, state_data))
+
+        Util.tokenService.exchangeAuthCodeToAccessToken(
+            code, Util.tokenService.getService(state_data["servicename"], clean=True), user=userId)
+
+        return "", 204
+
+    except Exception as e:
+        logger.exception(e)
+
+        abort(400)
