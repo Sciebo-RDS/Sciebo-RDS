928dc45ca74d999c342a5ec739746455d1b5ed04
Peter Heiss <peter.heiss@uni-muenster.de>
2020-06-16 14:40:46 +0200

    add docstring

--- RDS/circle2_use_cases/port/src/api/Exchange.py
+++ RDS/circle2_use_cases/port/src/api/Exchange.py
@@ -8,6 +8,10 @@ logger = logging.getLogger()
 
 
 def post():
+    """
+    This endpoint implements the same like redirect, but it returns 204 for succes and 400 if something goes wrong.
+    This enables, that integrations can trigger the serverpart of oauth workflow in rds by themselves.
+    """
     try:
         master_jwt = request.json.get("jwt")
         unverified = jwt.decode(master_jwt, verify=False)
@@ -15,8 +19,7 @@ def post():
         servicename = unverified.get("servicename")
         service = Util.tokenService.getService(servicename, clean=True)
 
-        master_data = jwt.decode(
-            master_jwt, service.client_secret, algorithms="HS256")
+        master_data = jwt.decode(master_jwt, service.client_secret, algorithms="HS256")
 
         logger.debug("jwt: {}, decoded: {}".format(master_jwt, master_data))
 
@@ -25,14 +28,15 @@ def post():
         logger.debug("code: {}, userId: {}".format(code, userId))
 
         state_jwt = master_data.get("state")
-        state_data = jwt.decode(
-            state_jwt, Util.tokenService.secret, algorithms="HS256")
+        state_data = jwt.decode(state_jwt, Util.tokenService.secret, algorithms="HS256")
 
-        logger.debug("state: {}, decoded state: {}".format(
-            state_jwt, state_data))
+        logger.debug("state: {}, decoded state: {}".format(state_jwt, state_data))
 
         Util.tokenService.exchangeAuthCodeToAccessToken(
-            code, Util.tokenService.getService(state_data["servicename"], clean=True), user=userId)
+            code,
+            Util.tokenService.getService(state_data["servicename"], clean=True),
+            user=userId,
+        )
 
         return "", 204
 
