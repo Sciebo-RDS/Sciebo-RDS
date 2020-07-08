e396eb307242829cd9bf88b13b837f9d93c017e3
Peter Heiss <peter.heiss@uni-muenster.de>
2020-07-08 10:11:03 +0200

    simplify env vars

--- RDS/circle1_adapters_and_ports/port_zenodo/src/server.py
+++ RDS/circle1_adapters_and_ports/port_zenodo/src/server.py
@@ -4,12 +4,22 @@ from __init__ import app, register_service
 import os
 
 
+redirect_uri = os.getenv("RDS_OAUTH_REDIRECT_URI", "")
+zenodo_address = os.getenv("ZENODO_ADDRESS", "https://sandbox.zenodo.org")
+zenodo_oauth_token_url = "{}/oauth/token".format(zenodo_address)
+zenodo_oauth_id = os.getenv("ZENODO_OAUTH_CLIENT_ID", "XY")
+zenodo_oauth_secret = os.getenv("ZENODO_OAUTH_CLIENT_SECRET", "ABC")
+
+zenodo_oauth_authorize = "{}/oauth/authorize%3Fredirect_uri={}&response_type=code&scope=deposit%3Awrite+deposit%3Aactions&client_id={}".format(
+    zenodo_address, redirect_uri, zenodo_oauth_id
+)
+
 register_service(
     "Zenodo",
-    os.getenv("ZENODO_OAUTH_AUTHORIZE_URL"),
-    os.getenv("ZENODO_OAUTH_ACCESS_TOKEN_URL"),
-    os.getenv("ZENODO_OAUTH_CLIENT_ID"),
-    os.getenv("ZENODO_OAUTH_CLIENT_SECRET")
+    zenodo_oauth_authorize,
+    zenodo_oauth_token_url,
+    zenodo_oauth_id,
+    zenodo_oauth_secret
 )
 
 # set the WSGI application callable to allow using uWSGI:
