41615f51a3529d71d7933f38dc36ce545dfb3048
Peter Heiss <peter.heiss@uni-muenster.de>
2020-07-09 10:48:53 +0200

    fix port test

--- RDS/circle2_use_cases/port/src/server.py
+++ RDS/circle2_use_cases/port/src/server.py
@@ -2,8 +2,9 @@
 
 from __init__ import bootstrap
 
-app = bootstrap("UseCaseTokenStorage", all=True)
+if __name__ == "__main__":
+    app = bootstrap("UseCaseTokenStorage", all=True)
 
-# set the WSGI application callable to allow using uWSGI:
-# uwsgi --http :8080 -w app
-app.run(port=8080, server="gevent")
+    # set the WSGI application callable to allow using uWSGI:
+    # uwsgi --http :8080 -w app
+    app.run(port=8080, server="gevent")
