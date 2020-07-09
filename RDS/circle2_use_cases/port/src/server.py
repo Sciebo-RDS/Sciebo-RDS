51f1794d81cef872b3e4d97eb488342858a9fcff
Peter Heiss <peter.heiss@uni-muenster.de>
2020-07-09 14:52:14 +0200

    rename port service

--- RDS/circle2_use_cases/port/src/server.py
+++ RDS/circle2_use_cases/port/src/server.py
@@ -2,7 +2,7 @@
 
 from __init__ import bootstrap
 
-app = bootstrap("UseCaseTokenStorage", all=True)
+app = bootstrap("UseCasePortService", all=True)
 
 # set the WSGI application callable to allow using uWSGI:
 # uwsgi --http :8080 -w app
