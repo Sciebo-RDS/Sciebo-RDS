0fae7584504530841c40e377d9f87721edd52ade
Peter Heiss <peter.heiss@uni-muenster.de>
2020-07-10 14:25:23 +0200

    fix js

--- plugins/ownCloud/rds/js/settings-admin.js
+++ plugins/ownCloud/rds/js/settings-admin.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 	$('#rds_submit').on('click', function (event) {
 		event.preventDefault();
 		var url = $("#cloud_url");
+		var app = "rds"
 		OC.AppConfig.setValue(app, url.attr('name'), url.val());
 	});
 
