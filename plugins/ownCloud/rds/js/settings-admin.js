3fb160b8f0553868480162ca507015044f1e4db8
Peter Heiss <peter.heiss@uni-muenster.de>
2020-07-13 14:28:59 +0200

    fix misusing admin panel
    if admin enters a trailing slash  in owncloud plugin, it will now remove

--- plugins/ownCloud/rds/js/settings-admin.js
+++ plugins/ownCloud/rds/js/settings-admin.js
@@ -2,8 +2,13 @@ $(document).ready(function () {
 
 	$('#rds_submit').on('click', function (event) {
 		event.preventDefault();
-		var url = $("#cloud_url");
 		var app = "rds"
+		var url = $("#cloud_url");
+		var urlValue = url.val();
+		if (urlValue.endsWith("/")) {
+			urlValue = urlValue.slice(0, -1);
+			url.val(urlValue);
+		}
 		OC.AppConfig.setValue(app, url.attr('name'), url.val());
 	});
 
