b97dd7caa26002eed7b2a2498b3038333d87bc6e
Peter Heiss <peter.heiss@uni-muenster.de>
2020-08-26 11:53:27 +0200

    Squashed commit of the following:
    commit 0cf01d423d4cafa9397927b0ce429d0cfe2e5dd7
    Author: Peter Heiss <peter.heiss@uni-muenster.de>
    Date:   Wed Aug 26 11:49:47 2020 +0200
    
        remove one logging
    
    commit 49b8efd565c8e9dc2a1b651122916e8bef9515fc
    Author: Peter Heiss <peter.heiss@uni-muenster.de>
    Date:   Wed Aug 26 11:36:40 2020 +0200
    
        add logging
    
    commit fd396c785308da33e15ecccdbe371d9d5f534f08
    Author: Peter Heiss <peter.heiss@uni-muenster.de>
    Date:   Wed Aug 26 11:36:24 2020 +0200
    
        setup dev config
    
    commit 5043e56124b6385c8473f2ffe7da6bd9767a3822
    Author: Peter Heiss <peter.heiss@uni-muenster.de>
    Date:   Wed Aug 26 11:03:07 2020 +0200
    
        fix admin template

--- plugins/ownCloud/rds/templates/settings-admin.php
+++ plugins/ownCloud/rds/templates/settings-admin.php
@@ -10,17 +10,20 @@ script('rds', 'settings-admin');
 
     <a target="_blank" rel="noreferrer" class="icon-info" title="<?php p($l->t('Open documentation')); ?>" href="https://www.research-data-services.org/doc/getting-started/config/"></a>
 
-    <p>
-        <form id="rds-settings">
+    <form id="rds-settings">
+        <p>
             <input type="text" name="cloudURL" id="cloud_url" class="text" <?php if (!empty($_["cloudURL"])) { ?> value="<?php print_unescaped($_['cloudURL']); ?>" <?php } ?> placeholder="<?php p($l->t('url to rds instance')); ?>" />
             <label for="cloudURL">
                 <?php p($l->t('Specify here the URL, where the ownCloud instance can find your RDS instance e.g. https://url-to-rds-in-k8s. Please leave out the trailing slash.')); ?>
             </label>
+        </p>
+        <p>
             <input type="text" name="oauthname" id="oauth_name" class="text" <?php if (!empty($_["oauthname"])) { ?> value="<?php print_unescaped($_['oauthname']); ?>" <?php } ?> placeholder="<?php p($l->t('name of oauth credentials')); ?>" />
             <label for="oauthname">
                 <?php p($l->t('Specify here the name of the oauth, which you entered in your ownCloud instance for user oauth authentification. It needs to be the same.')); ?>
             </label>
-        </form>
-        <button id="rds_submit" type="button" class="button"><?php p($l->t('Save')); ?></button>
+        </p>
+    </form>
+    <button id="rds_submit" type="button" class="button"><?php p($l->t('Save')); ?></button>
     </p>
 </div>
