39e1d5f4ea1303abbf6153dd3d87e648bceeba2e
Peter Heiss <peter.heiss@uni-muenster.de>
2020-09-22 15:42:35 +0200

    add todo

--- RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/zip.py
+++ RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/zip.py
@@ -2,5 +2,6 @@ from flask import jsonify
 
 
 def index():
+    # TODO: This should be done with creation for subfolder, because OSF supports it.
     data = {"needsZip": True}
     return jsonify(data)
