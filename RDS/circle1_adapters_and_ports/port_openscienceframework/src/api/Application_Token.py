543803b4290ebf8d31261352b774539c055c09f6
Peter Heiss <peter.heiss@uni-muenster.de>
2020-09-22 15:42:30 +0200

    working env vars

--- RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/Application_Token.py
+++ RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/Application_Token.py
@@ -3,13 +3,13 @@ from connexion_plus.Optimizer import FlaskOptimize
 import os
 
 redirect_uri = os.getenv("RDS_OAUTH_REDIRECT_URI", "")
-zenodo_address = os.getenv("ZENODO_ADDRESS", "https://sandbox.zenodo.org")
-zenodo_oauth_token_url = "{}/oauth/token".format(zenodo_address)
-zenodo_oauth_id = os.getenv("ZENODO_OAUTH_CLIENT_ID", "XY")
-zenodo_oauth_secret = os.getenv("ZENODO_OAUTH_CLIENT_SECRET", "ABC")
+osf_address = os.getenv("OPENSCIENCEFRAMEWORK_ADDRESS", "https://accounts.test.osf.io")
+osf_oauth_token_url = "{}/oauth2/token".format(osf_address)
+osf_oauth_id = os.getenv("OPENSCIENCEFRAMEWORK_OAUTH_CLIENT_ID", "XY")
+osf_oauth_secret = os.getenv("OPENSCIENCEFRAMEWORK_OAUTH_CLIENT_SECRET", "ABC")
 
-zenodo_oauth_authorize = "{}/oauth/authorize%3Fredirect_uri={}&response_type=code&scope%3Ddeposit%3Awrite%20deposit%3Aactions&client_id={}".format(
-    zenodo_address, redirect_uri, zenodo_oauth_id
+osf_oauth_authorize = "{}/oauth2/authorize?response_type=code&redirect_uri={}&client_id={}&scope=osf.full_write".format(
+    osf_address, redirect_uri, osf_oauth_id
 )
 
 
@@ -17,9 +17,9 @@ zenodo_oauth_authorize = "{}/oauth/authorize%3Fredirect_uri={}&response_type=cod
 def index():
     return jsonify(
         {
-            "authorize_url": zenodo_oauth_authorize,
-            "access_token_url": zenodo_oauth_token_url,
-            "client_id": zenodo_oauth_id,
-            "client_secret": zenodo_oauth_secret,
+            "authorize_url": osf_oauth_authorize,
+            "access_token_url": osf_oauth_token_url,
+            "client_id": osf_oauth_id,
+            "client_secret": osf_oauth_secret,
         }
     )
--- RDS/circle1_adapters_and_ports/port_openscienceframework/src/server.py
+++ RDS/circle1_adapters_and_ports/port_openscienceframework/src/server.py
@@ -10,7 +10,7 @@ osf_oauth_token_url = "{}/oauth2/token".format(osf_address)
 osf_oauth_id = os.getenv("OPENSCIENCEFRAMEWORK_OAUTH_CLIENT_ID", "XY")
 osf_oauth_secret = os.getenv("OPENSCIENCEFRAMEWORK_OAUTH_CLIENT_SECRET", "ABC")
 
-osf_oauth_authorize = "{}/oauth2/authorize?response_type=code&redirect_uri={}&client_id={}&scope=osf.full_read".format(
+osf_oauth_authorize = "{}/oauth2/authorize?response_type=code&redirect_uri={}&client_id={}&scope=osf.full_write".format(
     osf_address, redirect_uri, osf_oauth_id
 )
 
