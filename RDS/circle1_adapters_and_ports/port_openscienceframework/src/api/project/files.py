348b3933f4748961ae00ed902620f70948dfb8a2
Peter Heiss <peter.heiss@uni-muenster.de>
2020-09-23 12:52:15 +0200

    finish osf

--- RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/project/files.py
+++ RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/project/files.py
@@ -1,7 +1,8 @@
 import logging
 import os
 from lib.Util import require_api_key
-from flask import jsonify, request, g
+from flask import jsonify, request, g, abort
+from io import BytesIO
 
 logger = logging.getLogger()
 
@@ -9,30 +10,34 @@ logger = logging.getLogger()
 # FIXME: all endpoints need server tests, but POST cannot currently be tested through pactman, because it only supports json as content type
 @require_api_key
 def index(project_id):
-    return g.osf.get_files_from_deposition(project_id)
+    return jsonify(g.osf.project(project_id).storage().files())
 
 
 @require_api_key
 def get(project_id, file_id):
-    return g.osf.get_files_from_deposition(project_id)[file_id]
+    fileslist = list(g.osf.project(project_id).storage().files())
+    return jsonify(fileslist[file_id])
 
 
 @require_api_key
 def post(project_id):
     logger.debug("Read file from request")
-    file = request.files['file']
+    file = request.files["file"]
 
     req = request.form.to_dict()
     filename = req["filename"]
     logger.debug("file: {}, filename: {}".format(file, filename))
 
     logger.debug("Start file upload")
-    resp = g.osf.upload_new_file_to_deposition(
-        project_id, filename, file)
+    resp = (
+        g.osf.project(project_id)
+        .storage()
+        .create_file(filename, BytesIO(file.read()), force=True)
+    )
     logger.debug("Finished file upload")
 
     if resp:
-        return jsonify({"success": True})
+        return jsonify({"success": True}), 200
 
     else:
         raise ValueError("Upload failed.")
@@ -46,6 +51,12 @@ def patch(project_id, file_id):
 @require_api_key
 def delete(project_id, file_id=None):
     if file_id is None:
-        return g.osf.delete_all_files_from_deposition(project_id)
+        try:
+            for file in g.osf.project(project_id).storage().files():
+                file.remove()
+
+            return "", 200
+        except:
+            return abort(500)
 
     raise NotImplementedError()
--- RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/zip.py
+++ RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/zip.py
@@ -3,5 +3,5 @@ from flask import jsonify
 
 def index():
     # TODO: This should be done with creation for subfolder, because OSF supports it.
-    data = {"needsZip": True}
+    data = {"needsZip": False}
     return jsonify(data)
