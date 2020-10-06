821f7bb7836c29e1fb0392d07afae4b0b389a550
Peter Heiss <peter.heiss@uni-muenster.de>
2020-10-06 19:04:21 +0200

    logging error

--- RDS/circle1_adapters_and_ports/port_zenodo/src/api/project/project.py
+++ RDS/circle1_adapters_and_ports/port_zenodo/src/api/project/project.py
@@ -65,7 +65,9 @@ def index():
                 }
             )
 
-    except:
+    except Exception as e:
+        logger.error(e)
+        
         output = []
         for depo in depoResponse:
             output.append(
