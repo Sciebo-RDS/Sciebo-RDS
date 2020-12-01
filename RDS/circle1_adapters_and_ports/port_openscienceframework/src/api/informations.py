48d0ffc3c0c966964f5da40ebda29d990925873b
Peter Heiss <peter.heiss@uni-muenster.de>
2020-12-01 11:39:24 +0100

    fix transfermode exchange

--- RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/informations.py
+++ RDS/circle1_adapters_and_ports/port_openscienceframework/src/api/informations.py
@@ -4,7 +4,7 @@ from RDS import FileTransferMode, LoginMode
 def index():
     data = {
         "fileTransferArchive":"zip",
-        "fileTransferMode":FileTransferMode.active,
-        "loginMode":LoginMode.oauth
+        "fileTransferMode":FileTransferMode.active.value,
+        "loginMode":LoginMode.oauth.value
     }
     return jsonify(data)
