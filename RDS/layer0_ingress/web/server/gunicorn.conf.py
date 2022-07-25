f709cdc2606ded82f4e7bed23f66bdfe435903ca
Peter <peter.heiss@uni-muenster.de>
2022-07-25 23:46:02 +0200

    use varlog as folder

--- RDS/layer0_ingress/web/server/gunicorn.conf.py
+++ RDS/layer0_ingress/web/server/gunicorn.conf.py
@@ -4,8 +4,7 @@ import multiprocessing
 bind = "127.0.0.1:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 loglevel = "debug"
-logfile = "/app/logs/gunicorn.log"
-errorlog = "/app/logs/gunicorn.err.log"
+errorlog = "/var/log/gunicorn.err.log"
 capture_output = True
 worker_class = "eventlet"
 
