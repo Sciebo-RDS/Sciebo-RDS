ee0c10a98f1d7b86b9271259e36431597f4c435b
Peter <peter.heiss@uni-muenster.de>
2022-07-25 23:41:50 +0200

    try this out

--- RDS/layer0_ingress/web/server/gunicorn.conf.py
+++ RDS/layer0_ingress/web/server/gunicorn.conf.py
@@ -5,6 +5,7 @@ bind = "127.0.0.1:5000"
 workers = multiprocessing.cpu_count() * 2 + 1
 loglevel = "debug"
 logfile = "/app/logs/gunicorn.log"
+errorlog = "/app/logs/gunicorn.err.log"
 capture_output = True
 worker_class = "eventlet"
 
