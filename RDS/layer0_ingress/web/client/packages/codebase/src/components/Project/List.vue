39d3a53d3cabd0ce914ef86723087e6e1f9b3e57
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2022-09-02 16:33:11 +0200

    fix Projectlist height

--- RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/List.vue
+++ RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/List.vue
@@ -26,7 +26,7 @@
                 </v-sheet>
 
                 <!-- Project list content -->
-                <v-list style="overflow-y: auto; margin: 0; padding: 0; max-height: calc(100vh - 12.9em)">
+                <v-list style="overflow-y: auto; margin: 0; padding: 0; max-height: calc(100vh - 13.1em)">
                     <v-list-item-group>
                         
                         <div
