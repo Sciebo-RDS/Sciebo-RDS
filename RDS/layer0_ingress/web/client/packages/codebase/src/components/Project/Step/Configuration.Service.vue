Sciebo-RDS/Sciebo-RDS

commit 0be5bd13abb2a70ab9231fb1bb58add021df5908

[ui] service selection in project configuration (#212)
- fix visual bug

- fix multiselection of services

RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Step/Configuration.Service.vue
@@ -38,18 +38,19 @@
                     You have to <router-link to="services">connect a repository service</router-link> to continue.
                 </v-list-item-subtitle>
                 <v-list-item-content>
-                    <v-row v-if="userServiceList.length > 1" class="ma-1">
+                    <v-row v-if="userServiceList.filter((i) => i['implements'].includes('metadata')).length > 0" class="ma-1">
                         <v-col
                             v-for="p in userServiceList.filter((i) => i['implements'].includes('metadata'))"
                             :key="p.client_id"
                             cols="3"
-                            class="col-lg-2"
+                            class="col-lg-2 mr-5 rounded"
                             @click="selectPort(p)"
                             :style="isSelected(p) ? selectedStyle() : 'border: 1px solid transparent'"
                             align="center"
-                            style="display: grid; align-content: end; align: center">
+                            style="display: grid; align-content: end; align: center; max-width: 100%">
                             <v-img
                                 :src="p.icon"
+                                class="mb-2"
                                 :style="isSelected(p) ? '' : 'opacity: 0.3'"/>
                             <small style="vertical-align: bottom">
                                 {{ p['displayName'] }}
@@ -114,28 +115,23 @@ export default ({
     },
     methods: {
         isSelected(p) {
-            for (const s of this.loadedPortOut){
-                if (s.port === p.servicename)
-                    return true
-            }
-            return false
+
+            return this.loadedPortOut.filter(e => e.servicename === p.servicename).length > 0
         },
          selectPort(port) {
             if (this.isSelected(port)) {
-                return this.loadedPortOut = this.loadedPortOut.filter((p) => p.port !== port.servicename)
+                return this.loadedPortOut = []
             }
-            for (const oP of this.originalPortOut){
-                if (oP.port == port.servicename) {
-                    return this.loadedPortOut = this.originalPortOut.filter((p) => p.port === port.servicename)
+                if (this.originalPortOut.port === port.servicename){
+                    return this.loadedPortOut = [this.originalPortOut]
                 }
-            }
-            return this.loadedPortOut = [...this.loadedPortOut, ({"port": port.servicename, type: ["metadata"]})]
+            return this.loadedPortOut = [port]
          },
         selectedStyle() {
             if (this.$vuetify.theme.dark === true) {
-                return 'border: 1px solid #bada55; max-width: 100%'
+                return 'border: 1px solid #bada55;'
             }
-            return 'border: 1px solid black; max-width: 100%'
+            return 'border: 1px solid black;'
         }
 
     },

commit 026c65f999c9396c79eed7abb9b030c3415163b0

[ui] fix bug (refs #212) and layout
- fix bug introduced with 0be5bd1, refs #212

- fix height of project configuration card

- slight improvement to visibility of zenodo logo in dark mode

RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Step/Configuration.Service.vue
@@ -115,17 +115,18 @@ export default ({
     },
     methods: {
         isSelected(p) {
-
-            return this.loadedPortOut.filter(e => e.servicename === p.servicename).length > 0
+            return this.loadedPortOut.filter(e => e.port === p.servicename).length > 0
         },
          selectPort(port) {
             if (this.isSelected(port)) {
                 return this.loadedPortOut = []
             }
-                if (this.originalPortOut.port === port.servicename){
-                    return this.loadedPortOut = [this.originalPortOut]
-                }
-            return this.loadedPortOut = [port]
+            return this.loadedPortOut = [
+                {
+                    "port": port.servicename,
+                    properties: {"type": ["metadata"]}
+                    }
+                ]
          },
         selectedStyle() {
             if (this.$vuetify.theme.dark === true) {

RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Stepper.vue
@@ -31,7 +31,7 @@
           <v-card
             flat
             class="overflow-y-auto"
-            style="max-height: calc(100vh - 12.9em)"
+            style="height: calc(100vh - 12.9em)"
           >
             <StepConfiguration
               :project="project"

RDS/layer1_adapters_and_ports/port_zenodo/zenodo.svg
@@ -7,7 +7,7 @@
 <g>
 	<g>
 		<g>
-			<path fill="#FFFFFF" d="M145.301,18.875c-0.705-1.602-1.656-2.997-2.846-4.19c-1.189-1.187-2.584-2.125-4.188-2.805
+			<path fill="#393637" d="M145.301,18.875c-0.705-1.602-1.656-2.997-2.846-4.19c-1.189-1.187-2.584-2.125-4.188-2.805
 				c-1.604-0.678-3.307-1.02-5.102-1.02c-1.848,0-3.564,0.342-5.139,1.02c-0.787,0.339-1.529,0.74-2.225,1.205
 				c-0.701,0.469-1.357,1.003-1.967,1.6c-0.377,0.37-0.727,0.761-1.051,1.17c-0.363,0.457-0.764,1.068-0.992,1.439
 				c-0.281,0.456-0.957,1.861-1.254,2.828c0.041-1.644,0.281-4.096,1.254-5.472V2.768c0-0.776-0.279-1.431-0.84-1.965