2479f79bd39d508e568b38bf65040154c8439944
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-01-30 11:04:36 +0100

    More debugging

--- RDS/layer0_ingress/web/server/gunicorn.conf.py
+++ RDS/layer0_ingress/web/server/gunicorn.conf.py
@@ -2,7 +2,7 @@ from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 import multiprocessing
 
 bind = "127.0.0.1:5000"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1 #multiprocessing.cpu_count() * 2 + 1 # TODO: Debugging
 loglevel = "debug"
 capture_output = True
 worker_class = "eventlet"
