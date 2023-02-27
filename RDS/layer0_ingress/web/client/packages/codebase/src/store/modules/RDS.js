488676cf26b3d0d6a5d64d65ec2cfc5f944f6e2b
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2023-02-27 14:36:51 +0100

    add error handling to `getLoadedMetadataProfile`

--- RDS/layer0_ingress/web/client/packages/codebase/src/store/modules/RDS.js
+++ RDS/layer0_ingress/web/client/packages/codebase/src/store/modules/RDS.js
@@ -35,12 +35,17 @@ export default {
     },
     getLoadedMetadataProfile(state) {
       let loadedService = state.userservicelist.filter((i) => i.servicename == state.loadedProject.portOut[0].port)[0]
-      return loadedService["metadataProfile"]
+      try{
+        return loadedService["metadataProfile"]
+      }
+      catch {
+        return null
+      }
     },
     getOriginalFilePathForLoadedProject(state) {
       let p =  state.projectlist.filter((i) => i.researchIndex == state.loadedProject.researchIndex)[0]
       try {
-      return p.portIn[0]["properties"]["customProperties"]["filepath"]}
+        return p.portIn[0]["properties"]["customProperties"]["filepath"]}
       catch {
         return null
       }
