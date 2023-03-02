abc0d7376a3146194ede08a23cd37b34ca4a071b
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-03-02 15:46:48 +0100

    debugging round 7

--- RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Step/MetadataEditor.vue
+++ RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Step/MetadataEditor.vue
@@ -42,6 +42,7 @@ export default {
     ...mapGetters({
       ownCloudServicename: "getOwnCloudServername",
       loadedFilePath: "getLoadedFilePath",
+      loadedPortOut: "getLoadedPortOut",
       metadataProfile: "getLoadedMetadataProfile"
     }),
     editor() {
