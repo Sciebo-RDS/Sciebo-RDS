6a8210dec86e1dde93fe9a9563d91e8b4242240e
Daniel Müller <daniel.mueller@uni-muenster.de>
2023-03-03 12:26:10 +0100

    fixes

--- RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Stepper.vue
+++ RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Stepper.vue
@@ -224,7 +224,7 @@ export default {
         }
       }
     },
-    sendChanges() {
+    async sendChanges() {
       if (!!this.loadedResearchName && this.originalResearchName !== this.loadedResearchName) {
         this.$store.dispatch("changeResearchname", {
           researchIndex: this.loadedProject["researchIndex"],
