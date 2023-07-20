2e096a161b13f159262a5e126e5a84b4ffa2dd60
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2023-07-20 19:15:57 +0200

    Revert "parsing"
    This reverts commit b5109400064629b9b23549e5451dbaee40b00b3b.

--- RDS/layer2_use_cases/exporter/src/api/UserResearch.py
+++ RDS/layer2_use_cases/exporter/src/api/UserResearch.py
@@ -1,6 +1,5 @@
 from lib.Research import Research
 from flask import jsonify, current_app
-import json
 
 def index(user_id):
     pass
@@ -14,8 +13,8 @@ def post(user_id, research_id):
     success, messages, results = Research(userId=user_id, researchIndex=research_id, testing=current_app.config.get("TESTING")).synchronization()
     return jsonify({
         "success" : success,
-        "messages": json.loads(messages),
-        "fileSuccess": json.loads(results)
+        "messages": messages,
+        "fileSuccess": results
     })
 
 
