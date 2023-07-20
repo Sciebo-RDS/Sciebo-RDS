b5109400064629b9b23549e5451dbaee40b00b3b
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2023-07-20 18:50:51 +0200

    parsing

--- RDS/layer2_use_cases/exporter/src/api/UserResearch.py
+++ RDS/layer2_use_cases/exporter/src/api/UserResearch.py
@@ -1,5 +1,6 @@
 from lib.Research import Research
 from flask import jsonify, current_app
+import json
 
 def index(user_id):
     pass
@@ -13,8 +14,8 @@ def post(user_id, research_id):
     success, messages, results = Research(userId=user_id, researchIndex=research_id, testing=current_app.config.get("TESTING")).synchronization()
     return jsonify({
         "success" : success,
-        "messages": messages,
-        "fileSuccess": results
+        "messages": json.loads(messages),
+        "fileSuccess": json.loads(results)
     })
 
 
