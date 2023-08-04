70fb16c4298eaa83da09b2724732557a914f01ce
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2023-08-04 19:25:31 +0200

    add box-shadow to dialog-flex hack

--- RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Stepper.vue
+++ RDS/layer0_ingress/web/client/packages/codebase/src/components/Project/Stepper.vue
@@ -176,12 +176,13 @@
 </template>
 
 <style scoped>
-    .v-dialog--active {
+  .v-dialog--active {
       box-shadow: none;
-      
+      height: 100%;
     }
   .v-card{
-    margin: 0 auto;
+    margin: 5% auto;
+    box-shadow: 0 11px 15px -7px rgba(0,0,0,.2),0 24px 38px 3px rgba(0,0,0,.14),0 9px 46px 8px rgba(0,0,0,.12)!important;
   }
   
  </style>
