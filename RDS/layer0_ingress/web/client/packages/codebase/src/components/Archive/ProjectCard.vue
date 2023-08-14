27ac4a2689fd36196cde3131af96a360e8abd671
Juri Hößelbarth <hoesselbarth@uni-muenster.de>
2023-08-14 17:37:52 +0200

    styling

--- RDS/layer0_ingress/web/client/packages/codebase/src/components/Archive/ProjectCard.vue
+++ RDS/layer0_ingress/web/client/packages/codebase/src/components/Archive/ProjectCard.vue
@@ -5,13 +5,13 @@
           
           <v-card-text>
             <div></div>
-            <p class="text-h6 text--primary pb-3">
+            <p class="text-h6 text--primary pb-3" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;" :title="project.researchname">
                 {{ project.researchname}}
                 <v-divider class="my-2"></v-divider>
             </p>
             <v-div class="">
-            <v-row class="d-flex" flat  tile>
-              <v-col class="py-1 font-weight-medium" justify="left" flat tile>
+            <v-row class="d-flex mx-1" flat  tile>
+              <v-col class="py-1" justify="left" flat tile>
                 Repository-ID:
               </v-col>
 
@@ -20,8 +20,8 @@
               </v-col>
 
             </v-row>
-            <v-row class="d-flex" flat  tile>
-              <v-col class="py-1 font-weight-medium" justify="left" flat tile>
+            <v-row class="d-flex mx-1" flat  tile>
+              <v-col class="py-1" justify="left" flat tile>
                 Identifier (DOI):
               </v-col>
 
@@ -42,7 +42,7 @@
         @click=""
         
       >
-        Got to Publication
+        Go to Publication
       </v-btn>
     </v-card-actions>
         </v-card>
